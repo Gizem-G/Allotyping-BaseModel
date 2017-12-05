6e5e00c19cbe44289ac964c2a94330aab35d4c5f
Gizem-G <gizem.gueler@gmx.de>
2017-12-05 16:08:23 +0100

    Stratified test and folds

--- AllotypicPeptidesCV.R
+++ AllotypicPeptidesCV.R
@@ -7,12 +7,15 @@ library(rPython)
 require(pracma)
 library(matlab)
 
+set.seed(999)
+
 
 #Load sqlite db as dataframe
 options(stringsAsFactors = F)
 con = dbConnect(SQLite(), dbname='../Desktop/TueDB.sqlite')
 myQuery <- dbSendQuery(con, "SELECT * FROM Class1")
 df <- dbFetch(myQuery, n = -1)
+dbDisconnect()
 
 ###Load and clean database
 #df=read.csv("Projects/TumorSequencing/DB/TueDB_ClassI.csv", sep = ",")
@@ -38,11 +41,12 @@ df[which(df[,'HLA_A2']=="B07"),] <- NA
 df[which(df[,'HLA_B1']=="B"|df[,'HLA_B2']=="B"),] <- NA
 df[which(df[,'HLA_B1']=="C07"|df[,'HLA_B2']=="C07"),] <- NA
 df[which(df[,'HLA_B1']=="A66"|df[,'HLA_B2']=="A66"),] <- NA
+df[which(df[,'HLA_C1']=="B44"|df[,'HLA_C2']=="B44"),] <- NA
 df=na.omit(df)
 df$Dignity <- paste(df$Patient..Donor,df$Dignity)
 df['Sequence']<-sapply(df['Sequence'], toupper)
-forbidden_comments<-c('')
-df<-df[-is.element(df$Comment,forbidden_comments),]
+forbidden_comments<-c('HLA predicted','peptide-based HLA typing','from peptide scoring','peptides&proteoptyping','peptides&proteotyping','A68 maybe','Peptides (no DNA for typing)')
+df<-df[which(is.element(df$Comment,forbidden_comments)==FALSE),]
 
 ###Define Allotypes to differenciate
 allele_count_threshold<-9
@@ -115,20 +119,26 @@ label_matrix=label_matrix[shuffle,]
 rownames(label_matrix)=rownames(data_matrix)
 
 ### split test, train set and k-folds
-set.seed(999)
-TestSet <- sample(1:dim(data_matrix)[1], 55)
+samples<-c()
+for (allele in colnames(label_matrix)){
+  samples<-c(samples,which(label_matrix[allele]==1)[1])
+}
+samples<-unique(samples)
+samples<-c(samples,sample(which(is.element(seq(1,400),samples)==FALSE),50-length(samples)))
+
+TestSet <- samples
 data_test <- data_matrix[TestSet,]
 label_test <- label_matrix[TestSet,]
 #write.csv(as.data.frame(data_test),file = 'data_test.csv')
 #write.csv(as.data.frame(label_test),file = 'label_test.csv')
 data_matrix <- data_matrix[-TestSet,]
-flds<-createFolds(label_matrix$A01, k = 10, list = TRUE, returnTrain = FALSE)
+label_matrix <- label_matrix[-TestSet,]
 
 peptides_perc_list <- list()
-auc_list <- list()
+auc_list_96_5 <- list()
 
-percentage_threshold <- 0.95
-top_n_threshold <- 10
+percentage_threshold <- 0.96
+top_n_threshold <- 5
 python.load("../Desktop/Allotypic Peptides - Bachelorarbeit/Allotyping-BaseModel/auc_script.py")
 
 #prepare peplist list with samples as keys and peptide vector as values
@@ -141,6 +151,7 @@ for (row in seq(1,nrow(data_matrix))) {
 
 for (allele in All_Allotypes){
   print(allele)
+  flds<-createFolds(label_matrix[[allele]], k = 10, list = TRUE, returnTrain = FALSE)
   tp_matrix=data.frame(matrix(0, ncol = top_n_threshold, nrow= length(flds)))
   fp_matrix=data.frame(matrix(0, ncol = top_n_threshold, nrow= length(flds)))
   tn_matrix=data.frame(matrix(0, ncol = top_n_threshold, nrow= length(flds)))
@@ -185,7 +196,7 @@ for (allele in All_Allotypes){
   FPR = 1-(TN/(TN+FP))
   TPR=rev(c(1,TPR,0))
   FPR=rev(c(1,FPR,0))
-  auc_list[[allele]]<-trapz(FPR,TPR)
+  auc_list_96_5[[allele]]<-trapz(FPR,TPR)
 }
 
 
